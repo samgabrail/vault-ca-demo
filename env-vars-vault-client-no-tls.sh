7d32ad002e695427ada4d6f35c22354e950b12cb
samgabrail <sameh.gabrail@gmail.com>
2020-10-14 14:50:33 -0400

    more cleanup

--- env-vars-vault-client-no-tls.sh
+++ env-vars-vault-client-no-tls.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # You should source this file 'source ./env-vault-no-ssl.sh' so that 
 # the enivonment variables set here, stick in the original shell
-export VAULT_ADDR=http://127.0.0.1:8200
+
